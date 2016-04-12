5a97ece84beb3092f9327ebaa1038879806af42b
MishraSaurabh <saurabhmishra22@gmail.com>
2016-04-12 16:17:47 -0500

    Commit all little changes from today

---
+++ src/MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+            return "This is coming from Index action";
+        }
+                
+        public string Welcome(string name)
+        {
+            return "This is coming from Welcome action " + name;
+        }
+
+    }
+
+
+}
